8d5e312b65611ebf7b15a30dc23ebf4bdf7f9d89
b4tan <bryant.tan2103@gmail.com>
2024-11-17 23:04:38 -0800

    home progress

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,56 @@ function HomePage() {
     <div className="home-container">
       <div id ="home">
         <Hero />
+        <div className="relative flex justify-center items-center -mt-20 space-x-10" style = {{
+          marginLeft: "35px", marginRight:"35px",
+        }}>
+
+          <div
+          className="w-[300px] h-[200px] bg-gradient-to-r from-[#206c8c] to-[#1d997c] shadow-lg rounded-lg flex items-center justify-center"
+        >
+          <p
+            className="text-[#FAF9F6] text-md text-justify"
+            style={{
+              fontFamily: "Geneva",
+              opacity: 0.7, 
+              margin:"10px",
+            }}
+          > Simply upload a grading guideline and a student submission to start grading! Intelli. also supports the creation of your own grading guideline in-site.</p>
+        </div>
+
+        <div
+        className="w-[300px] h-[200px] shadow-lg rounded-lg flex items-center justify-center"
+        style={{
+          background: "radial-gradient(circle, #206c8c, #1d997c)",
+        }}
+      >
+        <p
+          className="text-[#FAF9F6] font-semibold text-lg text-center"
+          style={{
+            fontFamily: "Geneva",
+            opacity: 0.7,
+            margin: "10px",
+          }}
+        >
+          High accuracy! No need to worry
+        </p>
+      </div>
+
+          <div
+          className="w-[300px] h-[200px] bg-gradient-to-l from-[#206c8c] to-[#1d997c] shadow-lg rounded-lg flex items-center justify-center"
+        >
+          <p
+            className="text-[#FAF9F6] font-semibold text-lg text-center"
+            style={{
+              fontFamily: "Geneva",
+              opacity: 0.7,
+              margin: "10px",
+            }}
+          >
+            Saves time and effort by automation
+          </p>
+        </div>
+        </div>
       </div>
 
       <div id="autograder">
